2a5a3c91f4f2dc5bb21cef287323fae175d8174a
unknown <nagayewisdom@gmail.com>
2021-01-19 22:08:18 +0000

    added more ..

--- blacgods/settings.py
+++ blacgods/settings.py
@@ -120,7 +120,8 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
